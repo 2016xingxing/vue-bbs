4f587052c16f372d4313cc10e83b31ac72587767
李星星 <1348647935@qq.com>
2019-01-05 21:36:49 +0800

    注册页面的实现

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 import Vue from 'vue'
 // 引入 App.vue 的默认值
 import App from './App'
-
+// 引入 route.vue 的默认值
+import router from './router/index.js'
 // 设置 false 以阻止 Vue 在启动时生成生产提示
 Vue.config.productionTip = false
 
@@ -11,6 +12,7 @@ Vue.config.productionTip = false
 // 创建一个新的 Vue 实例
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
