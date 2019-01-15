b3628be7a25646d62fb99e661dbdea1ae6a20327
李星星 <1348647935@qq.com>
2019-01-15 16:42:52 +0800

    完成添加文章以及语法高亮

--- src/router/routes.js
+++ src/router/routes.js
@@ -18,18 +18,40 @@ export default [
     name: 'Login',
     component: () => import('@/views/auth/Login')
   },
-  // 编辑资料路由
+  // 个人编辑资料路由
   {
     path: '/users/1/edit',
     component: () => import('@/views/users/Edit.vue'),
     children: [
       {
+        // 为空的话，表示该页面作为默认子路由，导航到副页面的时候就加载
         path: '',
         name: 'EditProfile',
         component: () => import('@/views/users/Profile.vue'),
         // auth 为 true，标识当前路由需要登录才能访问
         meta: { auth: true }
+      },
+      {
+        path: '/users/1/edit_avatar',
+        name: 'EditAvatar',
+        component: () => import('@/views/users/Avatar.vue'),
+        meta: { auth: true }
+      },
+      {
+        path: '/users/1/edit_password',
+        name: 'EditPassword',
+        component: () => import('@/views/users/Password.vue'),
+        meta: { auth: true }
       }
     ]
   },
+  // 文章管理
+   {
+    path: '/articles/create',
+    name: 'Create',
+    component: () => import('@/views/articles/Create'),
+    meta: { auth: true }
+  },
+
+  
 ]
